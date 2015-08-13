dcc103ed06e4019fcc8054358e2cecc559d318f0
Gordon Tang <gordontang236@gmail.com>
2015-08-13 13:34:48 -0400

    Link About in footer

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'static_pages#home'
-
-  get    'about'   => 'static_pages#about'
+  root              'static_pages#home'
+  get  'about'   => 'static_pages#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
