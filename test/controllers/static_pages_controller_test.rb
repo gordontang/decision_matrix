6ffd63dab06267d230d9372fd00f99e3fc481f2c
Gordon Tang <gordontang236@gmail.com>
2015-08-13 12:09:32 -0400

    Add Home and About pages, and testing tools

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Decision Matrix"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Decision Matrix"
   end
 
 end
