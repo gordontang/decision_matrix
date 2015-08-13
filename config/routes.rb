63e25ee5b2a406585b6224e6aec7fdbc549613d1
Gordon Tang <gordontang236@gmail.com>
2015-08-13 13:20:42 -0400

    Center content on static pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'static_pages#home'
-  get 'static_pages/about'
+
+  get    'about'   => 'static_pages#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
