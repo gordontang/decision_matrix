d8ade58fb5745ceeccb710d2af9a468ffd8d6220
Gordon Tang <gordontang236@gmail.com>
2015-08-14 16:49:03 -0400

    Test Decision model

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -1,2 +1,3 @@
 class Decision < ActiveRecord::Base
+	validates :name, presence: true, length: { maximum: 50 }
 end
--- test/models/decision_test.rb
+++ test/models/decision_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class DecisionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@decision = Decision.new(name: "Sample decision")
+  end
+
+  test "should be valid" do
+  	assert @decision.valid?
+  end
+
+  test "name should be present" do
+  	@decision.name = "   "
+  	assert_not @decision.valid?
+  end
+
+  test "name should not be too long" do
+    @decision.name = "a" * 51
+    assert_not @decision.valid?
+  end
 end
