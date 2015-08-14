9a8c3fbf8e89638ae12a62a2867cdb80f856ee9f
Gordon Tang <gordontang236@gmail.com>
2015-08-14 16:36:42 -0400

    Update tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,11 +1,6 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get home" do
-    get :home
-    assert_response :success
-    assert_select "title", "Home | Decision Matrix"
-  end
 
   test "should get about" do
     get :about
