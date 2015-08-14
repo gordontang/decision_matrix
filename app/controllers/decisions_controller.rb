98f65715ecfee0f772776d1549802e80318ffaf6
Gordon Tang <gordontang236@gmail.com>
2015-08-14 18:44:26 -0400

    Add show decisions controller and view

--- app/controllers/decisions_controller.rb
+++ app/controllers/decisions_controller.rb
@@ -1,4 +1,9 @@
 class DecisionsController < ApplicationController
+
+	def show
+		@decision = Decision.find(params[:id])
+	end
+
   def new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'decisions/new'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -31,7 +29,7 @@ Rails.application.routes.draw do
   #     end
   #   end
 
-  #resources :decision
+  resources :decisions
 
   # Example resource route with sub-resources:
   #   resources :products do
