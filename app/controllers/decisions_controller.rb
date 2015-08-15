fb45306a75e1d0cf12441077f0e9c40e2095ae90
Gordon Tang <gordontang236@gmail.com>
2015-08-14 21:06:22 -0400

    Add form to Home page, and header to layout

--- app/controllers/decisions_controller.rb
+++ app/controllers/decisions_controller.rb
@@ -4,6 +4,24 @@ class DecisionsController < ApplicationController
 		@decision = Decision.find(params[:id])
 	end
 
-  def new
+	def new
+		@decision = Decision.new
+	end
+
+  def create
+  	@decision = Decision.new(decision_params)
+  	if @decision.save
+  		redirect_to root_url
+  	else
+  		render 'new'
+  	end
   end
+
+
+private
+
+  def decision_params
+      params.require(:decision).permit(:name)
+  end
+
 end
