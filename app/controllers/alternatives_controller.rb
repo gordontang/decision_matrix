d8958df1a980872493868b652463976c4ce6d371
Gordon Tang <gordontang236@gmail.com>
2016-03-05 14:58:44 -0500

    Enable creation of Alternatives via form

--- app/controllers/alternatives_controller.rb
+++ app/controllers/alternatives_controller.rb
@@ -5,14 +5,15 @@ class AlternativesController < ApplicationController
   end
 
   def create
-
   	@alternative = Alternative.new(alternative_params) #@alternative = current_decision.alternatives.build(alternative_params)
-  	if @decision.update_attributes(params[:decision])
-  		flash[:success] = "Alternative added!"
-  		redirect_to about_url ### Not final! Change to next input phase
-  	else
-  		render 'new'
-  	end
+  	@alternative.decision_id = current_decision.id
+    if @alternative.save
+      log_in Decision.find(current_decision.id)
+      flash[:success] = "Success! Added an alternative."
+      redirect_to Decision.find(current_decision.id)
+    else
+      render 'new'
+    end
   end
 
   def edit
